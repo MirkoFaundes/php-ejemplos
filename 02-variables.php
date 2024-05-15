6762d9a4310ce2d45f2370556c3bd4e3d3e56e27
Sergio Lauriente <sergiolauriente1@gmail.com>
2024-05-15 16:41:56 -0500

    Nuevos ejercicios

---
+++ 02-variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombres = "Mirko Andrey";
+$apellidos = "Faundes Lopez";
+$curso = "Backend developer web";
+$semestre = "Tercer Semestre";
+
+echo $nombres." ".$apellidos." ".$curso." ".$semestre;
+
+?>
