72c14cdb40a79f304672ab6049570dd701f774dd
Sergio Lauriente <sergiolauriente1@gmail.com>
2024-05-16 17:25:43 -0500

    Semaforo

---
+++ 10-booleanos.php
@@ -0,0 +1,33 @@
+<?php
+$luzEncendida=false;
+
+    if($luzEncendida)
+        echo "La luz esta encendida";
+    else
+        echo "La luz esta apagada";
+//caso 2
+
+$sintomas_covid=[
+    "tos"=>true,
+    "fiebre"=>false,
+    "dolor"=>false,
+    "perdida_olfato"=>false];
+
+if($sintomas_covid["tos"] &&
+$sintomas_covid["fiebre"]) {
+    echo "\nEsta enfermo tienes covid. Ve a centro de salud.";
+}
+else {
+    echo "\nAun no estas con covid.";
+}
+
+//caso 3, cuenta bancaria
+$saldo = 0;
+    if(!($saldo > 0)) {
+        echo "\nTiene saldo insuficiente";
+    }
+    else {
+        echo "\nTiene saldo";
+    }
+
+?>
