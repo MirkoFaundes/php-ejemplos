8859cba17eb11f65b5fefb8e08c640c845df8c10
Sergio Lauriente <sergiolauriente1@gmail.com>
2024-05-21 17:47:54 -0500

    Calculadora

---
+++ 12-clase.php
@@ -0,0 +1,42 @@
+<?php
+    //realizar una clase calculadora que tenga dos propiedades que represente los dos numeros a ingresar y
+    //6 metodos matematicos que debera tener : sumar,restar,mupltiplicar,dividir,potencia,raiz
+
+    class Calculadora {
+
+        public $primerNumero;
+        public $segundoNumero;
+
+        function __construct($num1 = 0, $num2 = 0) {
+            $this->primerNumero=$num1;
+            $this->segundoNumero=$num2;
+        }
+
+        public function sumar() {
+            return $this->primerNumero+$this->segundoNumero; 
+        }
+
+        public function restar() {
+            return $this->primerNumero-$this->segundoNumero;     
+        }
+
+        public function multiplicar() {
+            return $this->primerNumero*$this->segundoNumero;    
+        }
+
+        public function dividir() {
+            if($this->segundoNumero==0) return "UN ERROR";
+            else return $this->primerNumero/$this->segundoNumero;   
+        }
+
+        public function potencia() {
+            return pow ($this->primerNumero,$this->segundoNumero);    
+        }
+
+        public function raiz() {
+            return sqrt($this->primerNumero);    
+        }
+
+    }
+
+?>
---
+++ 12.1programa.php
@@ -0,0 +1,91 @@
+<?php
+    include("12-clase.php");
+
+    echo "******** BIENVENIDO A MI CALCULADORA ********\n";
+    echo "******** MENU DE OPCIONES ********\n";
+    echo "******** SUMAR (OPCION 1) ********\n";
+    echo "******** RESTAR (OPCION 2) ********\n";
+    echo "******** MULTIPLICAR (OPCION 3) ********\n";
+    echo "******** DIVIDIR (OPCION 4) ********\n";
+    echo "******** POTENCIA (OPCION 5) ********\n";
+    echo "******** RAIZ (OPCION 6) ********\n";
+    echo "******** SALIR (OPCION 0) ********\n";
+    echo "INGRESE LA OPCION :";
+    $opcion =fgets(STDIN);
+    switch ($opcion) {
+        case 1:
+            echo "ESCRIBA EL PRIMER NUMERO:";
+            $numero1 = fgets (STDIN);
+            echo "\nESCRIBA EL SEGUNDO NUMERO:";
+            $numero2 = fgets (STDIN);
+            $calculadora = new Calculadora($numero1,$numero2);
+            $resultado = $calculadora->sumar();
+            echo "\nLA SUMA ES :".$resultado;
+            # code...
+            break;
+
+        case 2:
+            echo "ESCRIBA EL PRIMER NUMERO:";
+            $numero1 = fgets (STDIN);
+            echo "\nESCRIBA EL SEGUNDO NUMERO:";
+            $numero2 = fgets (STDIN);
+            $calculadora = new Calculadora($numero1,$numero2);
+            $resultado = $calculadora->restar();
+            echo "\nLA RESTA ES :".$resultado;
+            # code...
+            break;
+
+        case 3:
+            echo "ESCRIBA EL PRIMER NUMERO:";
+            $numero1 = fgets (STDIN);
+            echo "\nESCRIBA EL SEGUNDO NUMERO:";
+            $numero2 = fgets (STDIN);
+            $calculadora = new Calculadora($numero1,$numero2);
+            $resultado = $calculadora->multiplicar();
+            echo "\nLA MULTIPLICACION ES :".$resultado;
+            # code...
+            break;
+            
+        case 4:
+            echo "ESCRIBA EL PRIMER NUMERO:";
+            $numero1 = fgets (STDIN);
+            echo "\nESCRIBA EL SEGUNDO NUMERO:";
+            $numero2 = fgets (STDIN);
+            $calculadora = new Calculadora($numero1,$numero2);
+            $resultado = $calculadora->dividir();
+            echo "\nLA DIVISION ES :".$resultado;
+            # code...
+            break;
+
+        case 5:
+            echo "ESCRIBA EL PRIMER NUMERO:";
+            $numero1 = fgets (STDIN);
+            echo "\nESCRIBA EL SEGUNDO NUMERO:";
+            $numero2 = fgets (STDIN);
+            $calculadora = new Calculadora($numero1,$numero2);
+            $resultado = $calculadora->potencia();
+            echo "\nLA POTENCIA ES :".$resultado;
+            # code...
+            break;
+
+        case 6:
+            echo "ESCRIBA EL PRIMER NUMERO:";
+            $numero1 = fgets (STDIN);
+            $calculadora = new Calculadora($numero1);
+            $resultado = $calculadora->raiz();
+            echo "\nLA RAIZ CUADRADA ES :".$resultado;
+            # code...
+            break;
+
+            case 0:
+                echo "MUCHAS GRACIAS POR ESTAR AQUI";
+                break;
+            default;
+                # code...
+                break;
+
+    }
+
+
+
+    ?>
