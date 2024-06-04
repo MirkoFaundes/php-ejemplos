62671d3746959b83a05a250eb25fde2037f81443
Sergio Lauriente <sergiolauriente1@gmail.com>
2024-06-04 17:56:30 -0500

    Buscar Paciente con AJAX

---
+++ 25.1-ajax.js
@@ -0,0 +1,20 @@
+function buscarPaciente() {
+    //const nombre = $('#nombre').val();
+    //alert("Buscando..."+nombre);
+    let datos = {
+        nombre : $nombre
+    };
+
+    $.ajax({
+        url :"26-ajax-pdo.php",
+        type : "post",
+        data : datos,
+        success : function(result) {
+            console.log(result);
+        }
+    })
+
+
+    return;
+
+}
